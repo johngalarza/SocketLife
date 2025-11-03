1467edb91770c90b61907f3ca2cb2b6320cb09cc
johngalarza <johngalarza1970@gmail.com>
2025-11-02 22:34:21 -0500

    feat: Initial config of sockets

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const http = require("http")
 const path = require('path');
+const socketIO = require("socket.io")
 const port = 3000;
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket)=>{
+    console.log('Player connected', socket.id);
+})
 
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, 'public'))
